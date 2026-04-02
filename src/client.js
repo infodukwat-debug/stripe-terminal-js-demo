36b3180119f867f33382ffc8d6417a18c71743cd
infodukwat-debug <infodukwat@gmail.com>
2026-04-02 04:47:02 +0200

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,4 +1,4 @@
-// Client for the example terminal backend: https://github.com/stripe/example-terminal-backend
+// Client for the example terminal backend
 class Client {
   constructor(url) {
     this.url = url;
@@ -6,37 +6,40 @@ class Client {
   }
 
   createConnectionToken() {
-    const formData = new URLSearchParams();
-    return this.doPost(this.url + "/connection_token", formData);
+    return this.doPost(this.url + "/connection_token", {});
   }
 
   registerDevice({ label, registrationCode, location }) {
-    const formData = new URLSearchParams();
-    formData.append("label", label);
-    formData.append("registration_code", registrationCode);
-    formData.append("location", location);
-    return this.doPost(this.url + "/register_reader", formData);
+    const body = {
+      label,
+      registration_code: registrationCode,
+      location
+    };
+    return this.doPost(this.url + "/register_reader", body);
   }
 
   createPaymentIntent({ amount, currency, description, paymentMethodTypes }) {
-    const formData = new URLSearchParams();
-    formData.append("amount", amount);
-    formData.append("currency", currency);
-    formData.append("description", description);
-    paymentMethodTypes.forEach((type) => formData.append(`payment_method_types[]`, type));
-    return this.doPost(this.url + "/create_payment_intent", formData);
+    const body = {
+      amount,
+      currency,
+      description,
+      payment_method_types: paymentMethodTypes
+    };
+    return this.doPost(this.url + "/create_payment_intent", body);
   }
 
   capturePaymentIntent({ paymentIntentId }) {
-    const formData = new URLSearchParams();
-    formData.append("payment_intent_id", paymentIntentId);
-    return this.doPost(this.url + "/capture_payment_intent", formData);
+    const body = {
+      payment_intent_id: paymentIntentId
+    };
+    return this.doPost(this.url + "/capture_payment_intent", body);
   }
 
   savePaymentMethodToCustomer({ paymentMethodId }) {
-    const formData = new URLSearchParams();
-    formData.append("payment_method_id", paymentMethodId);
-    return this.doPost(this.url + "/attach_payment_method_to_customer", formData);
+    const body = {
+      payment_method_id: paymentMethodId
+    };
+    return this.doPost(this.url + "/attach_payment_method_to_customer", body);
   }
 
   async listLocations() {
@@ -55,7 +58,10 @@ class Client {
   async doPost(url, body) {
     let response = await fetch(url, {
       method: "post",
-      body: body,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body),
     });
 
     if (response.ok) {
