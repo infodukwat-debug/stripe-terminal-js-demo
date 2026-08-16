143ff9812c26683de1800462db74f9c35859f30c
infodukwat-debug <infodukwat@gmail.com>
2026-08-16 19:34:39 +0200

    Update OverflowMenuItem.jsx

--- src/components/OverflowMenu/OverflowMenuItem.jsx
+++ src/components/OverflowMenu/OverflowMenuItem.jsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 
-import { css } from "emotion";
+import { css } from "@emotion/react";
 import Text from "../Text/Text.jsx";
 
 class OverflowMenuItem extends React.Component {
